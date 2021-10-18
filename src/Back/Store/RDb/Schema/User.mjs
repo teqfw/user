9603574f77c5a34db3c2bad2d24e49cb952cd7ca
Alex Gusev <alex@flancer64.com>
2021-10-18 16:18:22 +0300

    MF-115 Integrate @teqfw/user

--- src/Back/Store/RDb/Schema/User.mjs
+++ src/Back/Store/RDb/Schema/User.mjs
@@ -6,10 +6,41 @@ export default class TeqFw_User_Back_Store_RDb_Schema_User {
     id;
 }
 
+const ENTITY = '/user';
+/**
+ * @memberOf TeqFw_User_Back_Store_RDb_Schema_User
+ * @type {Object}
+ */
+const ATTR = {
+    DATE_CREATED: 'date_created',
+    ID: 'id'
+};
+
 // table name and columns names (entity and attributes) to use in queries to RDb
 TeqFw_User_Back_Store_RDb_Schema_User.A_DATE_CREATED = 'date_created';
 TeqFw_User_Back_Store_RDb_Schema_User.A_ID = 'id';
 TeqFw_User_Back_Store_RDb_Schema_User.ENTITY = 'user';
 
+/**
+ * @memberOf TeqFw_User_Back_Store_RDb_Schema_User
+ */
+class Meta {
+
+    constructor(spec) {
+        /** @type {TeqFw_User_Back_Defaults} */
+        const DEF = spec['TeqFw_User_Back_Defaults$'];
+        this.getEntityName = function () {
+            return `${DEF.SHARED.NAME}${ENTITY}`;
+        }
+
+        this.getAttributes = function () {
+            return ATTR;
+        }
+    }
+
+}
+
+
 // finalize code components for this es6-module
 Object.freeze(TeqFw_User_Back_Store_RDb_Schema_User);
+Object.freeze(ATTR);
