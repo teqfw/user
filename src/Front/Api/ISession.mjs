d5b3211d2417091758ea0650546a7456bbb1f678
Alex Gusev <alex@flancer64.com>
2021-11-03 14:50:54 +0200

    WF-514 Learn SSE

--- src/Front/Api/ISession.mjs
+++ src/Front/Api/ISession.mjs
@@ -2,6 +2,11 @@
  * @interface
  */
 export default class TeqFw_User_Front_Api_ISession {
+    /**
+     * Return 'true' if user is authenticated or redirect to sign in route otherwise.
+     * @param [router]
+     * @return {Promise<boolean>}
+     */
     async checkUserAuthenticated(router) {}
 
     async close(opts) {}
