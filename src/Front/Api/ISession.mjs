bd328cf607dd495a2cd534a9622c787d2a39a2b6
Alex Gusev <alex@flancer64.com>
2021-11-13 12:27:40 +0200

    DC-29 Add user to hollow main functionality

--- src/Front/Api/ISession.mjs
+++ src/Front/Api/ISession.mjs
@@ -4,10 +4,9 @@
 export default class TeqFw_User_Front_Api_ISession {
     /**
      * Return 'true' if user is authenticated or redirect to sign in route otherwise.
-     * @param [router]
      * @return {Promise<boolean>}
      */
-    async checkUserAuthenticated(router) {}
+    async checkUserAuthenticated() {}
 
     async close(opts) {}
 
@@ -20,10 +19,9 @@ export default class TeqFw_User_Front_Api_ISession {
 
     /**
      * Open session for the user and redirect to sign in route if user is not registered yet.
-     * @param router Vue Router
      * @return {Promise<void>}
      */
-    async open(router) {}
+    async open() {}
 
     /**
      * Close current session and reopen it (refresh user data from IDB).
@@ -31,6 +29,8 @@ export default class TeqFw_User_Front_Api_ISession {
      * @return {Promise<void>}
      */
     async reopen(route = null) {}
+
+    setRouter(router) { }
 
     setRouteToRedirect(route) {}
 
